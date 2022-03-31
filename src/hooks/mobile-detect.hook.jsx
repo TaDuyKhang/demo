6af7bc4a4d78c786a34b355a4305e80f64a06128
Bee <beeworker.dev@gmail.com>
2022-03-31 16:41:15 +0700

    update: trigger useMobileDetect hook when window resize

--- src/hooks/mobile-detect.hook.jsx
+++ src/hooks/mobile-detect.hook.jsx
@@ -1,10 +1,25 @@
 import {useState, useLayoutEffect} from "react";
 
+function debounce(func) {
+  let timer;
+  return function (event) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, 1200, event);
+  };
+}
+
 export function useMobileDetect() {
   const [isMobile, setIsMobile] = useState(true);
 
   useLayoutEffect(() => {
     setIsMobile(window.innerWidth < 768);
+
+    window.addEventListener(
+      "resize",
+      debounce(function () {
+        setIsMobile(window.innerWidth < 768);
+      })
+    );
   }, []);
 
   return isMobile;
