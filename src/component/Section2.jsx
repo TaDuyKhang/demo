6883a790713fa34cd62863c92eb05ffe384b8f8c
unknown <taduykhang2811999@gmail.com>
2022-04-03 01:54:36 +0700

    fix: section2 image

--- src/component/Section2.jsx
+++ src/component/Section2.jsx
@@ -2,17 +2,27 @@ import Border from "../img/section2/apple-iphone13pro-graphite-landscape 1.png";
 import Content from "../img/section2/nhieumat.png";
 import GGP from "../img/section2/gg.png";
 import AS from "../img/section2/xdg.png";
+import Collage from "../img/section2/ghep anh.png";
 import "../css/Section2.scss";
+import { useEffect, useState } from "react";
 
 function Section2() {
+  const { height, width } = useWindowDimensions();
+
   return (
     <section className="section2" id="news">
       <div className="content-container">
         <ul className="section2-box">
           <li className="banner-img">
             <div className="banner-image">
-              <img className="border" src={Border} alt="" />
-              <img className="content" src={Content} alt="" />
+              {width >= 768 && width <= 1439 ? (
+                <img className="collage" src={Collage} alt="" />
+              ) : (
+                <div>
+                  <img className="border" src={Border} alt="" />
+                  <img className="content" src={Content} alt="" />
+                </div>
+              )}
             </div>
           </li>
 
@@ -39,3 +49,37 @@ function Section2() {
 }
 
 export default Section2;
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  /*
+  Side by side comparison, to show equivalence 
+    Hook                Function
+  windowDimensions      this.state = { width: 0, height: 0 };
+  setWindowDimensions   updateWindowDimensions
+  */
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  /*
+    1. When useEffect is passed an empty array as its second argument, that means to run once onMount (similarly to componentDidMount)
+    2. return statements inside useEffects second argument are ran when the component will unMount
+*/
+
+  return windowDimensions;
+}
